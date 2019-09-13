abc0140b207cb1a4e24ba4935ca4fc08a8113046
FIC <FIC@info.local>
2019-09-13 14:52:15 -0300

    Exercicios de c#

---
+++ Exercicio-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sfixo,vfinal,nvendas;
+            Console.WriteLine("Qual é o salário fixo: ");
+            sfixo=float.Parse(Console.ReadLine());
+            Console.WriteLine("Qual o número total de vendas: ");
+            nvendas=float.Parse(Console.ReadLine());
+            vfinal=sfixo+(nvendas*1.05f);
+            Console.WriteLine($"O valor final é: {vfinal}");
+        }
+    }
+}
