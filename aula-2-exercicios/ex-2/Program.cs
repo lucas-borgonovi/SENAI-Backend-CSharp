bbc1bbba2999834deaa84c336430892d01d57c80
FIC <FIC@info.local>
2019-09-16 15:55:29 -0300

    Exercicios de estrutura condicional em c#

---
+++ aula-2-exercicios/ex-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float preco;
+            Console.WriteLine("Qual o preço? ");
+            preco=float.Parse(Console.ReadLine());
+            if(preco>100){
+                preco=preco-25;
+            }
+            Console.WriteLine(preco);
+        }
+    }
+}
---
+++ aula-2-exercicios/ex-3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int num1,num2;
+            Console.WriteLine("Digite o primeiro número: ");
+            num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            num2=int.Parse(Console.ReadLine());
+            if(num1>num2){
+                Console.WriteLine(num1);
+            }
+            else{
+                Console.WriteLine(num2);
+            }
+        }
+    }
+}
