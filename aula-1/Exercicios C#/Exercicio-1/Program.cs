399ff5592dbb39a40ddf83d480703d7ef64bd1f8
FIC <FIC@info.local>
2019-09-17 12:17:18 -0300

    Pasta Aula 1 em c#

---
+++ aula-1/Exercicios C#/Exercicio-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula1Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exercicio 1
+            float num,triplo;
+            Console.WriteLine("Digite um número: ");
+            num=float.Parse(Console.ReadLine());
+            triplo=num*3;
+            Console.WriteLine($"O triplo é: {triplo}");
+          
+            
+
+            
+
+
+
+        }
+    }
+}
