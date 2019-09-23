7e071125d857d13807cd6d4c7e77a39f9be7adb2
FIC <FIC@info.local>
2019-09-23 09:47:06 -0300

    Aulas de C#

--- aula-3/aula-3-repetição/ex-while-for/Program.cs
+++ aula-3/aula-3-repetição/ex-while-for/Program.cs
@@ -7,22 +7,22 @@ namespace aula_3
         static void Main(string[] args)
         {
              //LAÇO CONTADO-FOR-INCREMENTO(+)
-            for(int cont1=1;cont<=100;cont++){
+            // for(int cont1=1;cont<=100;cont++){
                 
-                // Verificamos se o número é diferente de par 
-                if(cont1%2!=0){
-                    Console.WriteLine("FOR 0-"+cont1);
-                }
-            }
-            //LAÇO CONTADO-FOR-DECREMENTO(-)
-            for(int cont2=100;cont>=0;cont--){
+            //     // Verificamos se o número é diferente de par 
+            //     if(cont1%2!=0){
+            //         Console.WriteLine("FOR 0-"+cont1);
+            //     }
+            // }
+            // //LAÇO CONTADO-FOR-DECREMENTO(-)
+            // for(int cont2=100;cont>=0;cont--){
                 
-                // Verificamos se o número é diferente de par 
-                if(cont2%2!=0){
-                    Console.WriteLine("FOR 100-0-"+cont2);
+            //     // Verificamos se o número é diferente de par 
+            //     if(cont2%2!=0){
+            //         Console.WriteLine("FOR 100-0-"+cont2);
 
-                }
-            } 
+            //     }
+            // } 
             //LAÇO CONDICIONAL-WHILE
             
              double nota=0,acumuladora=0;
