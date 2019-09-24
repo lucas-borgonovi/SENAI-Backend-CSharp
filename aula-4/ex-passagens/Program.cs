3171c670c5ef34c769c22e9ac2210b0dc2805312
FIC <FIC@info.local>
2019-09-24 11:58:11 -0300

    Aula de funçoes

---
+++ aula-4/ex-passagens/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace ex_passagens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Ok = true;
+            int Saida_Cadastro = 0;
+            int Entrada;
+            string Saida;
+            string[] Nome = new string[5];
+            string[] Origem = new string[5];
+            string[] Destino = new string[5];
+            string[] Data = new string[5];
+            DateTime[] Data_Ida = new DateTime[5];
+            DateTime[] Data_Volta = new DateTime[5];
+            int Quantidade_De_Passagens_Cadastrada = 0;
+
+            while(Ok) {
+                Console.WriteLine("========= Bem Vindo =========");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("\tOpções do Menu\n ( 1 - Cadastrar Passagem ) / ( 2 - Listar Passanges ) / ( 0 -  Sair )");
+                Entrada = int.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.White;
+
+                switch(Entrada) {
+                    case 0:
+                        Ok = false;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nSaindo do Menu......\n");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    break;    
+                    case 1:
+                        while(Saida_Cadastro <= 4 ) {
+                            Console.WriteLine("====== Cadastrar Passagem ======");
+                            
+                            if (string.IsNullOrEmpty(Nome[Saida_Cadastro])) {
+                                Console.Write("Informe o nome do passageiro: ");
+                                Nome[Saida_Cadastro] = Console.ReadLine();
+
+                                Console.Write("\nInforme a origem: ");
+                                Origem[Saida_Cadastro] = Console.ReadLine();
+
+                                Console.Write("\nInforme o destino: ");
+                                Destino[Saida_Cadastro] = Console.ReadLine();
+
+                                Console.Write("\nInforme a data do voo: ");
+                                Data[Saida_Cadastro] = Console.ReadLine();
+                            
+                                Quantidade_De_Passagens_Cadastrada++;
+
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("\nPassageiro Cadastrado Com Sucesso\n");
+                                Console.ForegroundColor = ConsoleColor.White;
+
+                            } else {
+                                int Cont = 0;
+                                for(int i = 0; i < Nome.Length; i++) {
+                                    if(!string.IsNullOrEmpty(Nome[i])) {
+                                        Cont++;
+                                    }
+                                }
+
+                                Console.Write("Informe o nome do passageiro: ");
+                                Nome[Cont] = Console.ReadLine();
+
+                                Console.Write("\nInforme a origem: ");
+                                Origem[Cont] = Console.ReadLine();
+
+                                Console.Write("\nInforme o destino: ");
+                                Destino[Cont] = Console.ReadLine();
+
+                                Console.Write("\nInforme a data do voo: ");
+                                Data[Cont] = Console.ReadLine();
+                            
+                                Quantidade_De_Passagens_Cadastrada++;
+
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("\nPassageiro Cadastrado Com Sucesso\n");
+                                Console.ForegroundColor = ConsoleColor.White;
+
+                            }
+                            
+                            Console.WriteLine("Deseja Cadastrar uma nova passagem? ( S / N )");
+                            Saida =Console.ReadLine();
+
+                            if(Saida == "n" || Saida == "N") {
+                               break;
+                            } 
+
+                            Saida_Cadastro++;
+                        }
+                    break;    
+                    case 2:
+                        if(!string.IsNullOrEmpty(Nome[0])) {
+                            Console.WriteLine("===== Lista de Passagens ======");
+                            for(int i = 0; i < Quantidade_De_Passagens_Cadastrada; i++) {
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                Console.WriteLine($"\nNome: {Nome[i]} \nOrigem: {Origem[i]} \nDestino: {Destino[i]} \nData do Voo: {Data[i]}\n");
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+
+                        } else {
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine("\nNão existem passagens cadastradas!!!\n");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    break;
+                    default: 
+                        Console.WriteLine("Opção Inválida");
+                        break;    
+                }
+            }
+        }
+    }
+}
+        
+    
+
---
+++ aula-5/aula-funcoes/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace aula_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Chamamos nossa primeira função
+            Console.WriteLine(BomDia());
+            //Chamamos a função do tipo void(sem retorno)
+            ImprimeDataHora();
+            
+            //Chamamos nossa função que calcula a média
+            double[] numeros={2,5,2,8,9,5};
+            Console.WriteLine(CalculaMedia(numeros));
+            //Chamamos a função de soma
+            Console.WriteLine(CalculaSoma(numeros));
+        }
+
+
+        /// <summary>
+        /// Função que escreve bom dia para o usuário
+        /// </summary>
+        /// <returns>Retorna Ola!Bom dia!</returns>
+
+        static string BomDia(){
+            return "Olá! Bom dia!";    
+        }
+
+
+        
+        /// <summary>
+        /// Retorna Data e hora atuais
+        /// </summary>
+        static void ImprimeDataHora(){
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(DateTime.Now.Hour.ToString());
+        }
+        /// <summary>
+        /// Função que mostra uma saudação de acordo com o horario atual
+        /// </summary>
+        /// <param name="saudacao">Um texto qualquer(sobrecarga de método)</param>
+        /// <returns>Saudação de acordo com o horário</returns>
+        static string BomDia(string saudacao){
+            int hora=DateTime.Now.Hour;
+            if(hora <=12 && hora>=6){
+                saudacao="Bom Dia";
+            }
+            else if(hora>12 && hora<=18){
+                saudacao="Boa Tarde";
+            }
+            else{
+                saudacao="Boa Noite";
+            }
+            return saudacao;
+        }
+        /// <summary>
+        /// Retorna um número -Teste de Sobrecarga
+        /// </summary>
+        /// <param name="numero">Um número inteiro qualquer</param>
+        /// <returns>Número passado como parâmetro</returns>
+        static int BomDia(int numero){
+            return numero;
+        }
+
+        static double CalculaMedia(double[] valores){
+            double resultado=0;
+
+            for(int i=0;i<valores.Length;i++){
+                resultado+=valores[i];
+            }
+            resultado=resultado/valores.Length;
+
+            return resultado;
+        }
+        /// <summary>
+        /// Calcula a soma de um vetor
+        /// </summary>
+        /// <param name="valores">vetor de doubles</param>
+        /// <returns>Soma de valores</returns>
+        static double CalculaSoma(double[] valores){
+            double resultado=0;
+
+            for(int i=0;i<valores.Length;i++){
+                resultado+=valores[i];
+            }
+            return resultado;
+        }
+
+    }
+}
